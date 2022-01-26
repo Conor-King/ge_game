be6eb385441f2dd8ef8b5fa43742802cdc86c9e8
Conor King <conor.king278@gmail.com>
2022-01-26 01:20:23 +0000

    Collisions implemented
    Have added the collisions for the top and bottom walls and paddles.

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -16,8 +16,22 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 
+Vector2f ballVelocity;
+bool server = false;
+
 CircleShape ball;
 RectangleShape paddles[2];
+
+void Reset() {
+    // Reset paddle position
+    paddles[0].setPosition(Vector2(10.f + paddleSize.x / 2.f, gameHeight / 2.f));
+    paddles[1].setPosition(Vector2(gameWidth - 10.f - paddleSize.x / 2.f, gameHeight / 2.f));
+
+    // Reset ball position
+    ball.setPosition(Vector2(gameWidth / 2.f - 10.f, gameHeight / 2.f - 10.f));
+
+    ballVelocity = { (server ? 100.f : -100.f), 60.f };
+}
 
 void Load() {
 
@@ -31,12 +45,7 @@ void Load() {
     ball.setRadius(ballRadius);
     ball.setOrigin(Vector2f(10, 10));
 
-    // Reset paddle position
-    paddles[0].setPosition(Vector2(10.f + paddleSize.x / 2.f, gameHeight / 2.f));
-    paddles[1].setPosition(Vector2(gameWidth - 10.f - paddleSize.x / 2.f, gameHeight / 2.f));
-
-    // Reset ball position
-    ball.setPosition(Vector2(gameWidth / 2.f - 10.f, gameHeight / 2.f - 10.f));
+    Reset();
 }
 
 void Update(RenderWindow &window) {
@@ -68,6 +77,63 @@ void Update(RenderWindow &window) {
         direction++;
     }
     paddles[0].move(Vector2(0.f, direction * paddleSpeed * dt));
+
+    float direction2 = 0.f;
+    if (Keyboard::isKeyPressed(controls[2])) {
+        direction2--;
+    }
+    if (Keyboard::isKeyPressed(controls[3])) {
+        direction2++;
+    }
+    paddles[1].move(Vector2(0.f, direction2 * paddleSpeed * dt));
+
+    // Handle ball movement
+    ball.move(ballVelocity * dt);
+
+    // Ball edge bounce
+    const float bx = ball.getPosition().x;
+    const float by = ball.getPosition().y;
+    if (by > gameHeight) { // Bottom Wall
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(Vector2(0.f, -10.f));
+    }
+    else if (by < 0) { // Top Wall
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(Vector2(0.f, 10.f));
+    }
+    else if (bx > gameWidth) { // Right Wall
+        Reset();
+    }
+    else if (bx < 0) { // Left Wall
+        Reset();
+    }
+    else if (
+        // ball is inline or behind the paddle
+        bx < paddles[0].getPosition().x + (paddleSize.x * 0.5) &&
+        // AND ball is below top edge
+        by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
+        // AND ball is above the bottom edge
+        by < paddles[0].getPosition().y + (paddleSize.y * 0.5)
+        ) {
+        ballVelocity.x *= -1.1f;
+        ballVelocity.y *= 1.1f;
+        ball.move(Vector2(10.f, 0.f));
+    }
+    else if (
+        // ball is inline or behind the paddle
+        bx > paddles[1].getPosition().x - (paddleSize.x * 0.5) &&
+        // AND ball is below top edge
+        by > paddles[1].getPosition().y - (paddleSize.y * 0.5) &&
+        // AND ball is above the bottom edge
+        by < paddles[1].getPosition().y + (paddleSize.y * 0.5)
+        ) {
+        ballVelocity.x *= -1.1f;
+        ballVelocity.y *= 1.1f;
+        ball.move(Vector2(-10.f, 0.f));
+    }
+
 }
 
 void Render(RenderWindow &window) {
